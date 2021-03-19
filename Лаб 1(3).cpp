e681cb188b055fbf0d66f15b036daf417f38ae72
VladislavSt467 <72553048+VladislavSt467@users.noreply.github.com>
2021-03-19 09:36:38 +0200

    Add files via upload

---
+++ Лаб 1(3).cpp
@@ -0,0 +1,18 @@
+﻿
+
+#include <iostream>
+
+int main()
+{  
+    setlocale(LC_ALL, "Ukrainian");
+    double a, b, c, d;
+    printf("Введи число, чтобы получить ответ в 8 степени:");
+    scanf_s("%1f", &a);
+    system("cls");
+    b = a * a;
+    c = b * b;
+    d = c * c;
+    printf("Число в 8 степени = %1f", d);
+}
+
+
