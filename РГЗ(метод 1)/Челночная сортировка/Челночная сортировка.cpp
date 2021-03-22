79a636d413dd12eb097d85adfa308cad09cc959c
VladislavSt467 <72553048+VladislavSt467@users.noreply.github.com>
2021-03-22 09:42:23 +0200

    Add files via upload

---
+++ РГЗ(метод 1)/Челночная сортировка/Челночная сортировка.cpp
@@ -0,0 +1,97 @@
+﻿#include <chrono>
+#include <algorithm>
+#include <time.h>
+#include <stdlib.h>
+#include <iostream>
+#include <iomanip>
+#define Povtor 100
+
+using namespace std;
+
+void shuttlesort(int* array, int Size);
+
+int main()
+{
+    setlocale(LC_ALL, "Ru");
+    const int Size = 45;
+    int  i;
+    cout << "Размер массива ->" << Size << endl;
+    int array[Size];
+    double time[Povtor];
+    for (int u = 0; u < Povtor; u++) 
+    {
+
+       /* cout << "Упорядоченный массив: " << endl;
+        for (i = 0; i < Size; i++) 
+        {
+           array[i] = i + 1;
+           cout << setw(4) << array[i];
+        }
+        cout << endl;*/
+
+       /* cout << "\nМассив, упорядоченный в обратном порядке: " << endl;
+        for (i = 0; i < Size; i++)
+        {
+            array[i] = Size - i;
+            cout << setw(4) << array[i];
+        }
+        cout << endl;*/
+
+        cout << "\nНеотсортированный массив: " << endl;
+        for (i = 0; i < Size; i++) 
+        {
+          array[i] = rand() % 50;
+          cout << setw(4) << array[i];
+        } 
+        cout << endl;
+
+
+        auto start = chrono::high_resolution_clock::now(); 
+        shuttlesort(array, Size);
+
+        auto end = chrono::high_resolution_clock::now(); 
+        chrono::duration<double> duration = end - start;
+
+        for (i = 0; i < Size; i++)
+        {
+            cout << setw(4) << array[i];
+        }
+        cout << endl << endl;
+
+        time[u] = duration.count();
+        cout << "Повторение №" << u << "   " << "Засеченное время = " << time[u] << endl;
+    }
+    double sum = 0;
+    double result;
+    sort(time, time + Povtor);
+
+    for (int u = 10; u < (Povtor - 10); u++)
+    {
+        sum += time[u];
+    }
+    cout << "Sum = " << sum << endl;
+    result = sum / (Povtor - 20);
+    cout << "Result =" << result << endl;
+    return 0;
+}
+
+ void shuttlesort(int* array, int Size)
+{
+    int temp, i, j;
+
+    for (i = 0; i < Size - 1; i++)      
+    {
+        if (array[i] > array[i + 1])
+        {
+            j = i - 1;
+            temp = array[i + 1];  
+            array[i + 1] = array[i];
+            while (j >= 0 && array[j] > temp)  
+            {
+                array[j + 101] = array[j];  
+                j = j - 1;
+            } 
+            array[j + 1] = temp;  
+        }      
+    }   
+}
---
+++ РГЗ(метод 2)/Сортировка подсчетом/Сортировка подсчетом.cpp
@@ -0,0 +1,101 @@
+﻿#include <stdio.h>
+#include <iostream>
+#include <iomanip>
+#include <chrono>
+#include <algorithm>
+#include <stdlib.h>
+#define Povtor 100
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    int max, min, L, k, N;
+    //const int N = 5;
+    const int D = 200;
+    double B[D];
+    cout << "Введите размер массива:" << endl;
+    cin >> N;
+    int* A = new int[N];
+    double time[Povtor];
+    for (int n = 0; n < Povtor; n++)
+    {
+        max = INT_MIN;
+        min = INT_MAX;
+       cout << "Неупорядоченный массив:" << endl;
+        for (int i = 0; i < N; i++) 
+        {
+            A[i] = rand() % 50;
+            cout << setw(4) << A[i];
+        }  
+        cout << endl;
+        /*cout << "Упорядоченный массив:" << endl;
+        for (int i = 0; i < N; i++)
+        {
+            A[i] = i + 1;
+            cout << setw(4) << A[i];
+        }
+        cout << endl;*/
+        /*cout << "Массив, упорядоченный в обратном порядке:" << endl;
+        for (int i = 0; i < N; i++)
+        {
+            A[i] = N - i;
+            cout << setw(4) << A[i];
+        }
+        cout << endl;*/
+        auto start = chrono::high_resolution_clock::now();
+        for (int i = 0; i < N; i++) 
+        {
+            if (A[i] > max)
+                max = A[i];
+            if (A[i] < min)
+                min = A[i];
+        }
+        L = max - min + 1;
+        int* B = new int[L];
+        for (int i = 0; i < L; i++)
+        {
+            B[i] = 0;
+        }
+        for (int i = 0; i < N; i++) 
+        {
+            B[A[i] - min]++;
+        }
+        k = 0;
+        for (int i = 0; i < L; i++) 
+        {
+            for (int j = 0; j < B[i]; j++)
+            {
+                A[k++] = i + min;
+            }
+        }
+        auto end = chrono::high_resolution_clock::now();
+        chrono::duration<double> duration = end - start;
+        time[n] = duration.count();
+
+        printf("\n№=  %d  Длительность:  %f\n", n, time[n]);
+
+        cout << "Массив после сортировки:" << endl;
+         for (int i = 0; i < N; i++)
+         {
+           cout << setw(4) << A[i];
+         }
+         cout << endl;
+        delete[] B;
+    }
+    double sum = 0;
+    double result;
+    sort(time, time + Povtor);
+
+    for (int u = 10; u < Povtor - 10; u++) 
+    {
+        sum += time[u];
+    }
+    cout << "Sum" << sum << endl;
+    result = sum / (Povtor - 20);
+    cout << "Result " << result << endl;
+
+    delete[] A;
+
+}
