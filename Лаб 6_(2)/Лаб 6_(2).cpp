e8a64fe553546b926c31aa6a150609b6f3747b28
VladislavSt467 <72553048+VladislavSt467@users.noreply.github.com>
2021-05-06 18:02:18 +0300

    Add files via upload

---
+++ Лаб 6_(2)/Лаб 6_(2).cpp
@@ -0,0 +1,127 @@
+﻿int main()
+{
+	setlocale(LC_ALL, "rus");
+tryagain:
+	int size = 255;
+
+
+
+
+
+	char* arr1 = new char[size];
+	char* arr2 = new char[size];
+
+
+	printf("\n\n");
+	printf("|---------------------------------------------------------------------------------------|");
+	printf("\n");
+	printf("Введите строку №1 (нажатие Enter завершит ввод): ");
+
+
+	/* Ввод строки 1*/
+	strEnter(arr1);
+
+	printf("Введенная строка: ");
+	printf("%s", arr1);
+
+	printf("\n");
+	/*Нахождение размера строки*/
+	int strl1 = strleng(arr1);
+
+	printf("Размер строки: %d", strl1);
+
+	printf("\n");
+	if (strl1 <= 0)
+	{
+		printf("\n");
+		printf("	DEBUG: Размер строки не удовлетворяет условию, попробуйте еще раз");
+		goto tryagain;
+	}
+	if (strl1 > size)
+	{
+		printf("\n");
+		printf("	DEBUG: Размер строки больше, чем ожидалось, попробуйте еще раз");
+		goto tryagain;
+	}
+	printf("\n\n");
+
+	/*--------------*/
+
+
+
+	/*            Ввод строки 2         */
+	printf("\n");
+	printf("Введите строку №2 (нажатие Enter завершит ввод): ");
+
+	strEnter(arr2);
+	printf("Введенная строка: ");
+	printf("%s", arr2);
+
+	printf("\n");
+	/*Нахождение размера строки*/
+	printf("Размер строки: %d", strleng(arr2));
+	printf("\n");
+
+	int strl2 = strleng(arr2);
+	if (strl2 <= 0)
+	{
+		printf("\n");
+		printf("	DEBUG: Размер строки не удовлетворяет условию, попробуйте еще раз");
+		goto tryagain;
+	}
+	if (strl2 > size)
+	{
+		printf("\n");
+		printf("	DEBUG: Размер строки больше, чем ожидалось, попробуйте еще раз");
+		goto tryagain;
+	}
+	printf("\n\n");
+
+
+
+
+
+	/*--------------*/
+
+tryagain2:
+	/*   Функция по задаче   */
+	printf("\n");
+	int p, n;
+	printf("Введите позицию, с которой следует начать замену: ");
+	scanf_s("%d", &p);
+	printf("\n");
+
+	if (p > strl1)
+	{
+		printf("\n");
+		printf("	DEBUG: Некоректный ввод позиции или кол-ва символов, попробуйте еще раз");
+		printf("\n");
+		goto tryagain2;
+	}
+tryagain3:
+	printf("Введите кол-во символов, которые следует заменить: ");
+	scanf_s("%d", &n);
+	printf("\n");
+
+	if (n > (strl1 | strl2))
+	{
+		printf("\n");
+		printf("	DEBUG: Некоректный ввод позиции или кол-ва символов, попробуйте еще раз");
+		printf("\n");
+		goto tryagain3;
+	}
+
+
+
+
+
+	Task(arr1, arr2, p, n);
+
+
+	printf("Итоговая строка : %s", arr1);
+	delete[]arr1;
+	delete[]arr2;
+	return 0;
+}
+
+
