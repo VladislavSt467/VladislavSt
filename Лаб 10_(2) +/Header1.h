b561236362dccf316457611c271f22b041e1b564
VladislavSt467 <72553048+VladislavSt467@users.noreply.github.com>
2021-12-21 09:18:37 +0200

    Add files via upload

--- Лаб 10_(2) +/Header1.h
+++ Лаб 10_(2) +/Header1.h
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-#define DEBUG
+//#define DEBUG
 
 class SUM
 {
@@ -12,7 +12,7 @@ private:
 public:
     int x;
     int y;
-
+    //конструктор с параметрами по умолчанию
     SUM(int valueX = 0, int valueY = 0) 
     {
         x = valueX;
@@ -20,7 +20,7 @@ public:
 
         s = 0;
     }
-
+    //функция инициализации x, y
     void SetX(int valueX)
     {
         x = valueX;
@@ -35,13 +35,13 @@ public:
     {
         return s;
     }
-
+    //функция вывода всех переменных класса
     void Print()
     {
         cout << "\nx = " << x << endl;
         cout << "\ny = " << y << endl;
     }
-
+    //функция подсчета суммы s и вывода результата на экран
     void countS()
     {
         s = x + y;
